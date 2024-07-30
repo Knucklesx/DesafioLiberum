f8fe80aeb1b37717e21a891a3a6639a89d560e4e
Gustavo Facchinetti <gcfacchi@gmail.com>
2024-07-30 00:21:42 -0300

    Validar CNPJ

--- backend/src/empresa/helper/CNPJValidate.ts
+++ backend/src/empresa/helper/CNPJValidate.ts
@@ -1,24 +1,23 @@
 export default async function CNPJValidate(cnpj: string) {
-  // const b = [6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
-  // const c = String(cnpj).replace(/[^\d]/g, '');
-  // if (c.length !== 14) {
-  //   return false;
-  // }
+  const b = [6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
+  const c = String(cnpj).replace(/[^\d]/g, '');
+  if (c.length !== 14) {
+    return false;
+  }
 
-  // if (/0{14}/.test(c)) {
-  //   return false;
-  // }
+  if (/0{14}/.test(c)) {
+    return false;
+  }
 
-  // let n = 0;
-  // for (let i = 0; i < 12; n += Number(c[i]) * b[++i]);
-  // if (Number(c[12]) != ((n %= 11) < 2 ? 0 : 11 - n)) {
-  //   return false;
-  // }
+  let n = 0;
+  for (let i = 0; i < 12; n += Number(c[i]) * b[++i]);
+  if (Number(c[12]) != ((n %= 11) < 2 ? 0 : 11 - n)) {
+    return false;
+  }
 
-  // n = 0;
-  // for (let i = 0; i <= 12; n += Number(c[i]) * b[i++]);
-  // if (Number(c[13]) != ((n %= 11) < 2 ? 0 : 11 - n)) return false;
-  console.log(cnpj);
+  n = 0;
+  for (let i = 0; i <= 12; n += Number(c[i]) * b[i++]);
+  if (Number(c[13]) != ((n %= 11) < 2 ? 0 : 11 - n)) return false;
 
   return true;
 }
